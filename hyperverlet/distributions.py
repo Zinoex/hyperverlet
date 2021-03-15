e87ff5941b16e70528e6e9a7b90861c9c54a4a5a
Frederik Mathiesen <frederik_mathies@hotmail.com>
2021-03-15 14:32:01 +0100

    Compute truncated gaussian distribution only using pytorch (no numpy)

--- hyperverlet/distributions.py
+++ hyperverlet/distributions.py
@@ -11,15 +11,14 @@ def parameterized_truncated_normal(uniform, mu, sigma, a, b):
     alpha_normal_cdf = normal.cdf(alpha)
     p = alpha_normal_cdf + (normal.cdf(beta) - alpha_normal_cdf) * uniform
 
-    p = p.numpy()
-    one = np.array(1, dtype=p.dtype)
-    epsilon = np.array(np.finfo(p.dtype).eps, dtype=p.dtype)
-    v = np.clip(2 * p - 1, -one + epsilon, one - epsilon)
-    x = mu + sigma * np.sqrt(2) * torch.erfinv(torch.from_numpy(v))
+    one = 1
+    epsilon = torch.finfo(p.dtype).eps
+    v = torch.clamp(2 * p - 1, -one + epsilon, one - epsilon)
+    x = mu + sigma * torch.sqrt(torch.tensor([2.0])) * torch.erfinv(v)
     x = torch.clamp(x, a, b)
 
     return x
 
 
 def sample_parameterized_truncated_normal(shape, mu, sigma, a, b):
-    return parameterized_truncated_normal(torch.from_numpy(np.random.uniform(0, 1, shape)), mu, sigma, a, b)
+    return parameterized_truncated_normal(torch.rand(shape), mu, sigma, a, b)
