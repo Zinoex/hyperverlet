ef42abed2ebd7f25ed28909e5e4b47f0df4334fc
Frederik Mathiesen <frederik_mathies@hotmail.com>
2021-03-17 12:23:13 +0100

    Fix phasespace plot bounds

--- hyperverlet/plotting/phasespace.py
+++ hyperverlet/plotting/phasespace.py
@@ -15,12 +15,19 @@ def plot_phasespace(q, p, show=True):
         plt.show()
 
 
-def init_phasespace_plot(ax, q, p, margine=1.05):
+def init_phasespace_plot(ax, q, p, margin=1.05):
     ax.set_title("Phase space")
     ax.set_xlabel("q")
     ax.set_ylabel("p")
-    ax.set_xlim(q.min() * margine, q.max() * margine)
-    ax.set_ylim(p.min() * margine, p.max() * margine)
+
+    q_half_range_ext = (q.max() - q.min()) * margin / 2
+    q_mid = (q.min() + q.max()) / 2
+
+    p_half_range_ext = (p.max() - p.min()) * margin / 2
+    p_mid = (p.min() + p.max()) / 2
+
+    ax.set_xlim(q_mid - q_half_range_ext, q_mid + q_half_range_ext)
+    ax.set_ylim(p_mid - p_half_range_ext, p_mid + p_half_range_ext)
 
 
 def update_phasespace_plot(ax, q, p, i):
