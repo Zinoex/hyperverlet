893abcca1b4dd4269cc110217032cc02483ea935
Frederik Mathiesen <frederik_mathies@hotmail.com>
2021-03-17 13:45:42 +0100

    Find parameters for spring mass

--- hyperverlet/models.py
+++ hyperverlet/models.py
@@ -32,18 +32,18 @@ class PendulumModel(nn.Module):
         super().__init__()
         self.h_dim = 16
 
-        self.model_q = SingleAxisMLP(6, self.h_dim)
-        self.model_p = SingleAxisMLP(6, self.h_dim)
+        self.model_q = SingleAxisMLP(4, self.h_dim)
+        self.model_p = SingleAxisMLP(4, self.h_dim)
 
     def forward(self, q, p, dq, dp, m, t, dt, length, include_q=True, include_p=True, **kwargs):
         if include_q:
-            hq = torch.cat([q, dq, p, dp, m, length], dim=-1)
+            hq = torch.cat([q, dq, m, length], dim=-1)
             hq = self.model_q(hq)
         else:
             hq = None
 
         if include_p:
-            hp = torch.cat([q, dq, p, dp, m, length], dim=-1)
+            hp = torch.cat([p, dp, m, length], dim=-1)
             hp = self.model_p(hp)
         else:
             hp = None
@@ -54,20 +54,20 @@ class PendulumModel(nn.Module):
 class SpringMassModel(nn.Module):
     def __init__(self):
         super().__init__()
-        self.h_dim = 16
+        self.h_dim = 64
 
-        self.model_q = SingleAxisMLP(3, self.h_dim)
-        self.model_p = SingleAxisMLP(4, self.h_dim)
+        self.model_q = SingleAxisMLP(5, self.h_dim)
+        self.model_p = SingleAxisMLP(5, self.h_dim)
 
     def forward(self, q, p, dq, dp, m, t, dt, length, k, include_q=True, include_p=True, **kwargs):
         if include_q:
-            hq = torch.cat([q, dq, m], dim=-1)
+            hq = torch.cat([q, dq, m, length, k], dim=-1)
             hq = self.model_q(hq)
         else:
             hq = None
 
         if include_p:
-            hp = torch.cat([p, dp, length, k], dim=-1)
+            hp = torch.cat([p, dp, m, length, k], dim=-1)
             hp = self.model_p(hp)
         else:
             hp = None
