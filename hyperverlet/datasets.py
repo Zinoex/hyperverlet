68a2fb7a779d47659dc9bb192deb5c87578e6d2d
Frederik Mathiesen <frederik_mathies@hotmail.com>
2021-04-08 09:09:17 +0200

    A little refactoring of datasets and experiments

---
+++ hyperverlet/datasets.py
@@ -0,0 +1,161 @@
+import torch
+from torch.utils.data import Dataset
+
+import numpy as np
+
+from hyperverlet.distributions import sample_parameterized_truncated_normal
+from hyperverlet.experiments import Pendulum, SpringMass, ThreeBodySpringMass, LennardJones
+from hyperverlet.timer import timer
+from hyperverlet.transforms import Coarsening
+
+
+class ExperimentDataset(Dataset):
+    def __init__(self, base_solver, duration, num_samples, num_configurations, coarsening_factor, sequence_length=None):
+        self.base_solver = base_solver
+        self.duration = duration
+        self.num_samples = num_samples
+        self.num_configurations = num_configurations
+        self.coarsening = Coarsening(coarsening_factor, num_samples)
+        self.sequence_length = sequence_length
+
+        self.trajectory = torch.linspace(0, duration, num_samples)
+        self.q, self.p = timer(lambda: self.base_solver.trajectory(self.experiment, self.q0, self.p0, self.mass, self.trajectory, **self.extra_args), 'data generation')
+
+        self.q, self.p, self.trajectory = self.coarsening(self.q, self.p, self.trajectory)
+
+        if self.sequence_length is not None:
+            assert self.coarsening.new_trajectory_length >= self.sequence_length, 'Trajectory length too short for coarsening'
+
+    def __len__(self):
+        if self.sequence_length is None:
+            return self.num_configurations
+        else:
+            assert self.coarsening.new_trajectory_length >= self.sequence_length, 'Trajectory length too short for coarsening'
+            configuration_length = self.coarsening.new_trajectory_length - self.sequence_length
+            return configuration_length * self.num_configurations
+
+    def __getitem__(self, idx):
+        if torch.is_tensor(idx):
+            idx = idx.tolist()
+
+        q = self.q
+        p = self.p
+        trajectory = self.trajectory
+
+        if self.sequence_length is None:
+            config_idx = idx
+        else:
+            configuration_length = self.coarsening.new_trajectory_length - self.sequence_length
+
+            config_idx, time_idx = idx // configuration_length, idx % configuration_length
+
+            q = q[time_idx:time_idx + self.sequence_length + 1]
+            p = p[time_idx:time_idx + self.sequence_length + 1]
+            trajectory = trajectory[time_idx:time_idx + self.sequence_length + 1]
+
+        return {
+            'q': q[:, config_idx],
+            'p': p[:, config_idx],
+            'mass': self.mass[config_idx],
+            'trajectory': trajectory,
+            'extra_args': self.config_extra_args(config_idx)
+        }
+
+    def config_extra_args(self, config_idx):
+        return self.extra_args
+
+
+class PendulumDataset(ExperimentDataset):
+    def __init__(self, base_solver, duration, num_samples, num_configurations, coarsening_factor, sequence_length=None,
+                 length_mean=1.0, length_std=0.5, mass_mean=0.9, mass_std=0.1, g=9.807):
+
+        self.experiment = Pendulum()
+        self.q0 = (torch.rand(num_configurations, 1) * 2 - 1) * (np.pi / 2)
+        self.p0 = torch.randn(num_configurations, 1) * 0.1
+        self.mass = torch.randn(num_configurations, 1) * mass_std + mass_mean
+        self.extra_args = {
+            'length': torch.randn(num_configurations, 1) * length_std + length_mean,
+            'g': torch.full((num_configurations, 1), g)
+        }
+
+        super().__init__(base_solver, duration, num_samples, num_configurations, coarsening_factor, sequence_length)
+
+    def config_extra_args(self, config_idx):
+        return {
+            'length': self.extra_args['length'][config_idx],
+            'g': self.extra_args['g'][config_idx]
+        }
+
+
+class SpringMassDataset(ExperimentDataset):
+    def __init__(self, base_solver, duration, num_samples, num_configurations, coarsening_factor, sequence_length=None, mass_mean=0.9, mass_std=0.1):
+
+        self.experiment = SpringMass()
+        length = sample_parameterized_truncated_normal((num_configurations, 1), 0.8, 0.35, 0.1, 1.5)
+        self.q0 = torch.rand(num_configurations, 1) * length * 2
+        self.p0 = torch.randn(num_configurations, 1) * 0.1
+        self.mass = torch.randn(num_configurations, 1) * mass_std + mass_mean
+        self.extra_args = {
+            'length': length,
+            'k': sample_parameterized_truncated_normal((num_configurations, 1), 0.8, 0.35, 0.1, 1.5)
+        }
+
+        super().__init__(base_solver, duration, num_samples, num_configurations, coarsening_factor, sequence_length)
+
+    def config_extra_args(self, config_idx):
+        return {
+            'length': self.extra_args['length'][config_idx],
+            'k': self.extra_args['k'][config_idx]
+        }
+
+
+class ThreeBodySpringMassDataset(ExperimentDataset):
+    def __init__(self, base_solver, duration, num_samples, num_configurations, coarsening_factor, sequence_length=None, mass_mean=0.9, mass_std=0.1):
+
+        num_particles = 3
+        num_springs = num_particles * (num_particles - 1) // 2
+        num_euclid = 2
+
+        self.experiment = ThreeBodySpringMass()
+        length = sample_parameterized_truncated_normal((num_configurations, num_springs), 0.8, 0.1, 0.1, 1.5)
+        self.q0 = torch.rand(num_configurations, num_particles, num_euclid) * length.max(dim=1, keepdim=True)[0].unsqueeze(2) * 2
+
+        # Don't judge, just accept
+        first_index = [i for i in range(0, num_particles) for j in range(i + 1, num_particles)]
+        second_index = [j for i in range(0, num_particles) for j in range(i + 1, num_particles)]
+
+        length_matrix = torch.zeros((num_configurations, num_particles, num_particles))
+        length_matrix[:, first_index, second_index] = length
+        length_matrix = length_matrix + length_matrix.transpose(1, 2)
+
+        k_matrix = torch.zeros((num_configurations, num_particles, num_particles))
+        k_matrix[:, first_index, second_index] = sample_parameterized_truncated_normal((num_configurations, num_springs), 0.8, 0.35, 0.1, 1.5)
+        k_matrix = k_matrix + k_matrix.transpose(1, 2)
+
+        self.p0 = torch.randn(num_configurations, num_particles, num_euclid) * 0.1
+        self.mass = torch.randn(num_configurations, num_particles, 1) * mass_std + mass_mean
+        self.extra_args = {
+            'length': length_matrix,
+            # The spring constant
+            'k': k_matrix
+        }
+
+        super().__init__(base_solver, duration, num_samples, num_configurations, coarsening_factor, sequence_length)
+
+    def config_extra_args(self, config_idx):
+        return {
+            'length': self.extra_args['length'][config_idx],
+            'k': self.extra_args['k'][config_idx]
+        }
+
+
+class LennardJonesDataset(ExperimentDataset):
+    def __init__(self, base_solver, duration, num_samples, num_configurations, coarsening_factor, sequence_length=None, eps=1, sigma=1, num_particles=10):
+        self.experiment = LennardJones(eps, sigma)
+        self.q0 = torch.rand((num_configurations, num_particles, 3)) * 5
+        p0 = (torch.rand_like(self.q0) * 2 - 1) * torch.tensor([0.03]).sqrt()
+        self.p0 = p0 - p0.mean(axis=1)
+        self.mass = torch.ones((num_configurations, num_particles, 1))
+        self.extra_args = {}
+
+        super().__init__(base_solver, duration, num_samples, num_configurations, coarsening_factor, sequence_length)
--- hyperverlet/experiments.py
+++ hyperverlet/experiments.py
@@ -1,75 +1,25 @@
+import abc
+
 import torch
-from math import pi
-
 from torch import nn
-from torch.utils.data import Dataset
-
-from hyperverlet.distributions import sample_parameterized_truncated_normal
-from hyperverlet.timer import timer
-from hyperverlet.transforms import Coarsening
-from hyperverlet.utils import torch_to_numpy, send_to_device
 
 
-class ExperimentDataset(Dataset):
-    def __init__(self, base_solver, duration, num_samples, num_configurations, coarsening_factor, sequence_length=None):
-        self.base_solver = base_solver
-        self.duration = duration
-        self.num_samples = num_samples
-        self.num_configurations = num_configurations
-        self.coarsening = Coarsening(coarsening_factor, num_samples)
-        self.sequence_length = sequence_length
-
-        self.trajectory = torch.linspace(0, duration, num_samples)
-        self.q, self.p = timer(lambda: self.base_solver.trajectory(self.experiment, self.q0, self.p0, self.mass, self.trajectory, **self.extra_args), 'data generation')
-
-        self.q, self.p, self.trajectory = self.coarsening(self.q, self.p, self.trajectory)
-
-        if self.sequence_length is not None:
-            assert self.coarsening.new_trajectory_length >= self.sequence_length, 'Trajectory length too short for coarsening'
-
-    def __len__(self):
-        if self.sequence_length is None:
-            return self.num_configurations
-        else:
-            assert self.coarsening.new_trajectory_length >= self.sequence_length, 'Trajectory length too short for coarsening'
-            configuration_length = self.coarsening.new_trajectory_length - self.sequence_length
-            return configuration_length * self.num_configurations
-
-    def __getitem__(self, idx):
-        if torch.is_tensor(idx):
-            idx = idx.tolist()
-
-        q = self.q
-        p = self.p
-        trajectory = self.trajectory
-
-        if self.sequence_length is None:
-            config_idx = idx
-        else:
-            configuration_length = self.coarsening.new_trajectory_length - self.sequence_length
-
-            config_idx, time_idx = idx // configuration_length, idx % configuration_length
-
-            q = q[time_idx:time_idx + self.sequence_length + 1]
-            p = p[time_idx:time_idx + self.sequence_length + 1]
-            trajectory = trajectory[time_idx:time_idx + self.sequence_length + 1]
-
-        return {
-            'q': q[:, config_idx],
-            'p': p[:, config_idx],
-            'mass': self.mass[config_idx],
-            'trajectory': trajectory,
-            'extra_args': self.config_extra_args(config_idx)
-        }
-
-    def config_extra_args(self, config_idx):
-        return self.extra_args
-
-
-class Pendulum(nn.Module):
+class Experiment(nn.Module, abc.ABC):
     def forward(self, q, p, m, t, **kwargs):
         return self.dq(p, m, t, **kwargs), self.dq(q, m, t, **kwargs)
 
+    def dq(self, p, m, t, length, **kwargs):
+        return p / m
+
+    @abc.abstractmethod
+    def dp(self, q, m, t, length, g, **kwargs):
+        raise NotImplementedError()
+
+    def shift(self, q, **kwargs):
+        return q
+
+
+class Pendulum(Experiment):
     def dq(self, p, m, t, length, **kwargs):
         return p / (m * length ** 2)
 
@@ -77,82 +27,17 @@ class Pendulum(nn.Module):
         return -m * g * length * torch.sin(q)
 
 
-class PendulumDataset(ExperimentDataset):
-    def __init__(self, base_solver, duration, num_samples, num_configurations, coarsening_factor, sequence_length=None,
-                 length_mean=1.0, length_std=0.5, mass_mean=0.9, mass_std=0.1, g=9.807):
-
-        self.experiment = Pendulum()
-        self.q0 = (torch.rand(num_configurations, 1) * 2 - 1) * (pi / 2)
-        self.p0 = torch.randn(num_configurations, 1) * 0.1
-        self.mass = torch.randn(num_configurations, 1) * mass_std + mass_mean
-        self.extra_args = {
-            'length': torch.randn(num_configurations, 1) * length_std + length_mean,
-            'g': torch.full((num_configurations, 1), g)
-        }
-
-        super().__init__(base_solver, duration, num_samples, num_configurations, coarsening_factor, sequence_length)
-
-    def config_extra_args(self, config_idx):
-        return {
-            'length': self.extra_args['length'][config_idx],
-            'g': self.extra_args['g'][config_idx]
-        }
-
-
-class SpringMass(nn.Module):
-    def forward(self, q, p, m, t, **kwargs):
-        return self.dq(p, m, t, **kwargs), self.dp(q, m, t, **kwargs)
-
-    def dq(self, p, m, t, **kwargs):
-        return p / m
-
+class SpringMass(Experiment):
     def dp(self, q, m, t, length, k, **kwargs):
         return -k * (q - length)
 
 
-class SpringMassDataset(ExperimentDataset):
-    def __init__(self, base_solver, duration, num_samples, num_configurations, coarsening_factor, sequence_length=None, mass_mean=0.9, mass_std=0.1):
-
-        self.experiment = SpringMass()
-        length = sample_parameterized_truncated_normal((num_configurations, 1), 0.8, 0.35, 0.1, 1.5)
-        self.q0 = torch.rand(num_configurations, 1) * length * 2
-        self.p0 = torch.randn(num_configurations, 1) * 0.1
-        self.mass = torch.randn(num_configurations, 1) * mass_std + mass_mean
-        self.extra_args = {
-            'length': length,
-            'k': sample_parameterized_truncated_normal((num_configurations, 1), 0.8, 0.35, 0.1, 1.5)
-        }
-
-        super().__init__(base_solver, duration, num_samples, num_configurations, coarsening_factor, sequence_length)
-
-    def config_extra_args(self, config_idx):
-        return {
-            'length': self.extra_args['length'][config_idx],
-            'k': self.extra_args['k'][config_idx]
-        }
-
-
-class ThreeBodySpringMass(nn.Module):
-    def forward(self, q, p, m, t, **kwargs):
-        return self.dq(p, m, t, **kwargs), self.dp(q, m, t, **kwargs)
-
-    def dq(self, p, m, t, **kwargs):
-        return p / m
-
-    def dp(self, q, m, t, length, k, **kwargs):
+class BasePairPotential(Experiment):
+    def dp(self, q, m, t, **kwargs):
         disp = self.displacement(q)
         r = self.distance(disp)
 
-        return self.force(r, disp, k, length).sum(axis=-2)
-
-    def force(self, r, disp, k, length):
-        num_particles = k.size(1)
-        r_prime = (r + torch.eye(num_particles, num_particles, device=r.device)).unsqueeze(-1)
-        direction = disp / r_prime
-
-        offset = r - length
-
-        return -2 * (k * offset).unsqueeze(-1) * direction
+        return self.force(r, disp, **kwargs).sum(axis=-2)
 
     def displacement(self, q):
         a = torch.unsqueeze(q, -2)
@@ -164,69 +49,22 @@ class ThreeBodySpringMass(nn.Module):
         return disp.norm(dim=-1)
 
 
-class ThreeBodySpringMassDataset(ExperimentDataset):
-    def __init__(self, base_solver, duration, num_samples, num_configurations, coarsening_factor, sequence_length=None, mass_mean=0.9, mass_std=0.1):
+class ThreeBodySpringMass(BasePairPotential):
+    def force(self, r, disp, k, length, **kwargs):
+        num_particles = k.size(1)
+        r_prime = (r + torch.eye(num_particles, num_particles, device=r.device)).unsqueeze(-1)
+        direction = disp / r_prime
 
-        num_particles = 3
-        num_springs = num_particles * (num_particles - 1) // 2
-        num_euclid = 2
+        offset = r - length
 
-        self.experiment = ThreeBodySpringMass()
-        length = sample_parameterized_truncated_normal((num_configurations, num_springs), 0.8, 0.1, 0.1, 1.5)
-        self.q0 = torch.rand(num_configurations, num_particles, num_euclid) * length.max(dim=1, keepdim=True)[0].unsqueeze(2) * 2
-
-        # Don't judge, just accept
-        first_index = [i for i in range(0, num_particles) for j in range(i + 1, num_particles)]
-        second_index = [j for i in range(0, num_particles) for j in range(i + 1, num_particles)]
-
-        length_matrix = torch.zeros((num_configurations, num_particles, num_particles))
-        length_matrix[:, first_index, second_index] = length
-        length_matrix = length_matrix + length_matrix.transpose(1, 2)
-
-        k_matrix = torch.zeros((num_configurations, num_particles, num_particles))
-        k_matrix[:, first_index, second_index] = sample_parameterized_truncated_normal((num_configurations, num_springs), 0.8, 0.35, 0.1, 1.5)
-        k_matrix = k_matrix + k_matrix.transpose(1, 2)
-
-        self.p0 = torch.randn(num_configurations, num_particles, num_euclid) * 0.1
-        self.mass = torch.randn(num_configurations, num_particles, 1) * mass_std + mass_mean
-        self.extra_args = {
-            'length': length_matrix,
-            # The spring constant
-            'k': k_matrix
-        }
-
-        super().__init__(base_solver, duration, num_samples, num_configurations, coarsening_factor, sequence_length)
-
-    def config_extra_args(self, config_idx):
-        return {
-            'length': self.extra_args['length'][config_idx],
-            'k': self.extra_args['k'][config_idx]
-        }
+        return -2 * (k * offset).unsqueeze(-1) * direction
 
 
-class LenardJones(nn.Module):
-    def __init__(self, eps=1, sigma=1):
-        super().__init__()
-
-        self.eps = eps
-        self.sigma = sigma
-
-    def forward(self, q, p, m, t):
-        # q = q.requires_grad_(True)
-        # q.requires_grad = True
-
-        disp = self.displacement(q)
-        r = self.distance(disp)
-        # acceleration = force / mass
-        dq = p / m
-        dp = self.force(r, disp).sum(axis=1)
-
-        return dq, dp
-
-    def force(self, r, disp):
-        prefix = 48 * self.eps
-        sigma12 = self.sigma ** 12
-        sigma6 = self.sigma ** 6
+class LennardJones(BasePairPotential):
+    def force(self, r, disp, eps, sigma, **kwargs):
+        prefix = 48 * eps
+        sigma12 = sigma ** 12
+        sigma6 = sigma ** 6
 
         r = r.fill_diagonal_(1)
 
@@ -243,25 +81,6 @@ class LenardJones(nn.Module):
         # scalar_force = torch.autograd.grad(energy, r, retain_graph=True)[0].fill_diagonal_(0)
         # return -scalar_force.unsqueeze(2) * (disp / r.fill_diagonal_(1).unsqueeze(2))
 
-    def potential(self, r):
-        return (4 * self.eps * ((self.sigma / r) ** 12 - (self.sigma / r) ** 6)).fill_diagonal_(0)
+    def potential(self, r, eps, sigma, **kwargs):
+        return (4 * eps * ((sigma / r) ** 12 - (sigma / r) ** 6)).fill_diagonal_(0)
 
-    def displacement(self, q):
-        a = torch.unsqueeze(q, 1)
-        b = torch.unsqueeze(q, 0)
-        return a - b
-
-    def distance(self, disp):
-        return disp.norm(dim=2)
-
-
-class LennardJonesDataset(ExperimentDataset):
-    def __init__(self, base_solver, duration, num_samples, num_configurations, coarsening_factor, sequence_length=None, eps=1, sigma=1, num_particles=10):
-        self.experiment = LenardJones(eps, sigma)
-        self.q0 = torch.rand((num_configurations, num_particles, 3)) * 5
-        p0 = (torch.rand_like(self.q0) * 2 - 1) * torch.tensor([0.03]).sqrt()
-        self.p0 = p0 - p0.mean(axis=1)
-        self.mass = torch.ones((num_configurations, num_particles, 1))
-        self.extra_args = {}
-
-        super().__init__(base_solver, duration, num_samples, num_configurations, coarsening_factor, sequence_length)
