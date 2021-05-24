ed434a6ee1b810201736240c8ccb7887fdf74f81
anders <amads15@student.aau.dk>
2021-05-24 09:17:38 +0200

    main updated

--- hyperverlet/main.py
+++ hyperverlet/main.py
@@ -17,6 +17,7 @@ from hyperverlet.utils.misc import seed_randomness, load_config, save_pickle, fo
 systems = ['pendulum', 'pendulum20', 'pendulum40', 'pendulum60', 'pendulum80', 'pendulum100',
            'spring_mass', 'spring_mass25', 'spring_mass50', 'spring_mass100', 'spring_mass200',
            'three_body_spring_mass', 'three_body_spring_mass25', 'three_body_spring_mass50', 'three_body_spring_mass100', 'three_body_spring_mass200']
+
 config_paths = {
     'integrator_comparison': [
         'configurations/integrator_comparison/{system}/euler.json',
