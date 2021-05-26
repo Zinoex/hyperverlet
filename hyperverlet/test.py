4d092ef5d4bbed9fb09ba38323ff3efc3bafdffd
Frederik Mathiesen <frederik_mathies@hotmail.com>
2021-05-26 11:42:05 +0200

    Add option to save train inference

--- hyperverlet/test.py
+++ hyperverlet/test.py
@@ -7,14 +7,27 @@ from hyperverlet.utils.measures import print_qp_loss
 from hyperverlet.utils.misc import send_to_device, torch_to_numpy
 
 
-def test(solver: nn.Module, dataset, device, config):
+def test(solver: nn.Module, train_dataset, test_dataset, device, config):
     solver.eval()
-    criterion = nn.MSELoss()
-
     train_args = config["train_args"]
+
+    test_results = inference(solver, test_dataset, device, train_args, label='test')
+
+    if train_args.get('save_train_inference', False):
+        train_results = inference(solver, train_dataset, device, train_args, label='train')
+        test_results['train'] = train_results
+
+    return test_results
+
+
+@torch.no_grad()
+def inference(solver: nn.Module, dataset, device, train_args, label):
     num_workers = train_args["num_workers"]
 
     loader = DataLoader(dataset, num_workers=num_workers, pin_memory=device.type == 'cuda', batch_size=len(dataset))
+
+    def run():
+        solver.trajectory(dataset.experiment, q_base[0], p_base[0], mass, trajectory, **extra_args)
 
     with torch.no_grad():
         batch = next(iter(loader))
@@ -25,8 +38,8 @@ def test(solver: nn.Module, dataset, device, config):
         trajectory = batch['trajectory'].transpose(0, 1).contiguous().to(device, non_blocking=True)
         extra_args = send_to_device(batch['extra_args'], device, non_blocking=True)
 
-        (q, p), inference_time = timer(lambda: solver.trajectory(dataset.experiment, q_base[0], p_base[0], mass, trajectory, **extra_args), 'solving', return_time=True)
-        print_qp_loss(q, p, q_base, p_base)
+        (q, p), inference_time = timer(run, f'solving {label}', return_time=True)
+        print_qp_loss(q, p, q_base, p_base, label=f'final loss {label}')
 
     return {
         "inference_time": inference_time,
